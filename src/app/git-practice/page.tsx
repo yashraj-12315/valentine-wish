3050e7982cbdb8511f501e7f512a0526cce820f6
Yash Raj <yashraj4586@gmail.com>
2026-02-12 18:39:42 +0530

    Revert "add git practice page"
    This reverts commit 92a067273701831dc5a78079055ef4ab87d97fd7.

--- src/app/git-practice/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Page = () => {
-  return (
-    <div className='h-screen flex justify-center items-center'>This is the Git Practice Page</div>
-  )
-}
-
-export default Page

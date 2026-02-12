92a067273701831dc5a78079055ef4ab87d97fd7
Yash Raj <yashraj4586@gmail.com>
2026-02-12 18:28:33 +0530

    add git practice page

---
+++ src/app/git-practice/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Page = () => {
+  return (
+    <div className='h-screen flex justify-center items-center'>This is the Git Practice Page</div>
+  )
+}
+
+export default Page
